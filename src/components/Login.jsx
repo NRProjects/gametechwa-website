3d8e96cf1c44504795b39ee2cdaed92b684f9c3a
NRProjects <56905917+NRProjects@users.noreply.github.com>
2023-08-14 17:07:11 -0700

    Prototyped login design

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,27 @@
-
+import './Login.css';
 
 function Login() {
 
     return (
-        <>
-            <div>
-                Login
+        <div className='login-page-container'>
+            <div className='login-box-container'>
+                <label className='login-box-label'>
+                    Username
+                </label>
+                <input className='login-box-input'>
+                </input>
+                <label className='login-box-label'>
+                    Password
+                </label>
+                <input className='login-box-input'>
+                </input>
+
+                <button className='login-box-button'>LOGIN</button>
+                <button className='login-box-button'>FORGOT PASSWORD</button>
+
+                
             </div>
-        </>
+        </div>
     );
 }
 
