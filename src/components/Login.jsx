68c0533c62fe9e9b5fe45265a64c404e8281b63e
NRProjects <56905917+NRProjects@users.noreply.github.com>
2023-08-14 18:17:08 -0700

    Added alerts for login testing

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,39 @@
+import React, { useState } from 'react';
 import './Login.css';
 
 function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+
+        if (!username || !password) {
+            alert("Invalid credentials");
+            return;
+        }
+
+        alert('Valid credentials');
+    };
 
     return (
-        <div className='login-page-container'>
-            <div className='login-box-container'>
+        <div className='login-page-container' onSubmit={handleLogin}>
+            <form className='login-box-container'>
                 <label className='login-box-label'>
                     Username
                 </label>
-                <input className='login-box-input'>
-                </input>
+                <input className='login-box-input' value={username} onChange={(e) => setUsername(e.target.value)}/>
+
                 <label className='login-box-label'>
                     Password
                 </label>
-                <input className='login-box-input'>
-                </input>
+                <input type='password' className='login-box-input' value={password} onChange={(e) => setPassword(e.target.value)}/>
 
-                <button className='login-box-button'>LOGIN</button>
+                <button type='submit' className='login-box-button'>LOGIN</button>
                 <button className='login-box-button'>FORGOT PASSWORD</button>
 
                 
-            </div>
+            </form>
         </div>
     );
 }
